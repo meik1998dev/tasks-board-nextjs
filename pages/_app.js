c998fa9200e482fc9d7b26e9be9bbd7a147579d2
mohamad <m.aleik@darsoft.net>
2022-01-26 23:06:16 +0200

    A: add roboto font

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,9 @@ function MyApp({ Component, pageProps }) {
             <link
                href='https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700&display=swap'
                rel='stylesheet'></link>
+            <link
+               href='https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;1,300;1,400&display=swap'
+               rel='stylesheet'></link>
          </Head>
          <Component {...pageProps} />
       </>
