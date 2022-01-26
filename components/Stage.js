60fc9d3558fabfe83a76f47999ac7d6be775901b
mohamad <m.aleik@darsoft.net>
2022-01-26 22:34:12 +0200

    A: create Stage component

--- components/Stage.js
+++ components/Stage.js
@@ -1,21 +1,44 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-export const Stage = () => {
+export const Stage = (props) => {
    return (
-      <div>
+      <StageContainer>
          <StageHeader>
             <Title>Stage Title</Title>
-            {/* <Description>description</Description> */}
+            <Description>description</Description>
          </StageHeader>
-      </div>
+      </StageContainer>
    );
 };
 
-const StageHeader = styled.div`
-   border-left: 8px solid black;
-   height: 56px;
+const StageContainer = styled.div`
+   display: flex;
+   flex-direction: column;
+   align-items: flex-start;
+   padding: 32px;
+
+   position: absolute;
+   min-width: 414px;
+   min-height: 334px;
+   left: 37px;
+   top: 265px;
+
+   /* Neutral/400 */
+
+   background: #f8f9fa;
+   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+   border-radius: 16px;
 `;
+
+const StageHeader = styled.div((props) => ({
+   borderLeft: '8px solid black',
+   paddingLeft: '15px',
+   height: '56px',
+   color: props.color || 'black',
+   display: 'flex',
+   flexDirection: 'column',
+}));
 
 const Title = styled.p`
    font-family: Open Sans;
@@ -23,13 +46,20 @@ const Title = styled.p`
    font-weight: 800;
    font-size: 24px;
    line-height: 33px;
-   /* identical to box height */
-
+   letter-spacing: 0.08em;
+   margin: 0;
+`;
+const Description = styled.p`
+   font-family: Roboto;
+   font-style: normal;
+   font-weight: normal;
+   font-size: 13px;
+   line-height: 15px;
    display: flex;
    align-items: center;
    letter-spacing: 0.08em;
 
-   /* Neutral/200 */
+   /* Neutral/300 */
 
-   color: #313131;
+   color: #757575;
 `;
